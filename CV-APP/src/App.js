fd637372dde14c6afe27b0b697a08231692faa6f
abhi2322 <abhishekjamwal34@gmail.com>
2021-06-16 18:16:30 +0530

    Adding edit and preview mode

--- CV-APP/src/App.js
+++ CV-APP/src/App.js
@@ -1,5 +1,42 @@
+import React,{Component} from 'react';
+import EditMode from './components/edit_mode';
+import PreviewMode from './components/preview_mode';
 
-
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      editMode:true
+    }
+  }
+ edit=()=>{
+   
+   this.setState({
+     editMode:true
+   })
+  }
+  preview=()=>{
+    this.setState({
+      editMode:false
+    })
+  }
+ 
+  render(){
+    let mode=null
+    if(this.state.editMode){
+      mode=<EditMode />
+    }else{
+      mode=<PreviewMode />
+    }
+    return(
+      <div>
+      <button onClick={this.edit}>Edit</button>
+      <button onClick={this.preview}>preview</button>
+        <div>{mode}</div>
+      </div>
+    )
+  }
+}
 
 
 export default App;
---
+++ CV-APP/src/components/preview_mode.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class PreviewMode extends Component{
+    render(){
+        return(
+            <div>
+            Preview MODE
+            </div>
+        )
+    }
+}
+
+export default PreviewMode
